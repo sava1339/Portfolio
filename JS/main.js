e3a486d9f809896460bc423b2fc27e171e475965
unknown <zsava0090@gmail.com>
2022-12-09 22:43:20 +0500

    s

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,4 @@
 const winHei = window.screen.height;
-function main(){
-    ()=> new Promise((res,rej)=>{
-        res(document.querySelector(main))
-    });
-}
-async function autoHeight(){
-    const main = await main();
-    main.style.cssText = `
-        min-height:${winHei};
-    `;
-}
+console.log(winHei);
+const main = document.querySelector("main");
+main.style.minHeight = winHei;
