43cf8357fb647c84a3333c65b833d1b3e7502502
unknown <zsava0090@gmail.com>
2022-12-19 18:10:50 +0500

    ss

--- JS/posterPage.js
+++ JS/posterPage.js
@@ -1,4 +1,4 @@
-const imgCommodity= document.querySelector(".img-commudity");
+const imgCommodity= document.querySelector(".img-commodity");
 const nameCommodity= document.querySelector(".name-commodity");
 imgCommodity.querySelector("img").src = localStorage.urlpage;
 nameCommodity.textContent = localStorage.namepage;
