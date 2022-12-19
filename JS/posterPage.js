a67f42462d163703222ca25b98700e5ecb865869
unknown <zsava0090@gmail.com>
2022-12-19 20:06:48 +0500

    fixEnter

--- JS/posterPage.js
+++ JS/posterPage.js
@@ -1,4 +1,13 @@
 const imgCommodity= document.querySelector(".img-commodity");
 const nameCommodity= document.querySelector(".name-commodity");
-imgCommodity.querySelector("img").src = localStorage.urlpage;
-nameCommodity.textContent = `Плакат ${localStorage.namepage}`;
+if(sessionStorage.urlpage){
+    imgCommodity.querySelector("img").src = sessionStorage.urlpage;
+    nameCommodity.textContent = `Плакат ${sessionStorage.namepage}`;
+}else{
+    imgCommodity.querySelector("img").src = localStorage.urlpage;
+    nameCommodity.textContent = `Плакат ${localStorage.namepage}`;
+}
+sessionStorage.urlpage =localStorage.urlpage;
+sessionStorage.namepage =localStorage.namepage;
+
+
